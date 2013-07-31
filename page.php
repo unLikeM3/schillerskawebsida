8eac62cd489b27fb49a3021ca9aaad4467e7616e
John Eriksson <johnreriksson@gmail.com>
2013-07-31 17:17:03 +0200

    fixed signup layout

--- page.php
+++ page.php
@@ -7,7 +7,7 @@
 						<?php if(have_posts()) : while(have_posts()) : the_post();?>
 							<h1><?php the_title(); ?></h1>
 							<p><?php the_content(); ?></p>
-						<?php endwhile; endif;?>
+						<?php endwhile; endif; ?>
 					</div>
 				</div>
 			</div>
