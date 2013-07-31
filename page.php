f31b42acaf4d92765ced59aaefd524111e5b236f
John Eriksson <johnreriksson@gmail.com>
2013-07-31 16:22:40 +0200

    Added page.php and configured that so pages look different from singles, also added permalinks to 'Bli Medlem' and 'Kontakt', both being pages

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+	<div class="container">
+		<div class="row">
+			<div class="span12 mt45">
+				<div>
+					<div class="postcontainer padded">
+						<?php if(have_posts()) : while(have_posts()) : the_post();?>
+							<h1><?php the_title(); ?></h1>
+							<p><?php the_content(); ?></p>
+						<?php endwhile; endif;?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+<?php get_footer(); ?>
