8e1a86fec7dcd234b534f1fde85d098608a394e5
John Eriksson <johnreriksson@gmail.com>
2013-08-03 15:20:40 +0200

    failed last commit, redo

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+	$host	= 'wp-182898.mysql.binero.se';
+	$user	= '182898_pu91310';
+	$pass	= 'Sch1llNad';
+	$db		= '182898-wp';
+
+	$con = mysql_connect($host, $user, $pass);
+
+	if (!$con) {
+		die("Cannot connect to database");
+	}else{
+		mysql_select_db($db) or die('Cannot select database');
+	}
+
+?>
---
+++ memberhandler.php
@@ -0,0 +1,28 @@
+<?php 
+	require 'connect.php';
+
+	$fname = $_POST['fornamn'];
+	$lname = $_POST['efternamn'];
+	$pnr = $_POST['persnr'];
+	$email = $_POST['email'];
+
+	$sql = "INSERT INTO medlemmar VALUES ('$fname', '$lname', '$pnr', '$email')";
+
+	if($fname != null && $lname != null){
+		if (is_numeric($pnr)) {
+			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+				if (mysql_query($sql)) {
+					echo "Du är nu medlem! <br> Klicka <a href='../../../'>här</a> för att gå vidare!";
+				}else{
+					die(mysql_error());
+				}
+			}else{
+				die('Du måste fylla i din email!');
+			}
+		}else {
+			die('Personnummer måste vara i YYYYMMDDXXXX-format!');
+		}
+	}else{
+		die('Du måste fylla i förnamn/efternamn!');
+	}
+?>
