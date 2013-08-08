dded7752cdc39510ca9ce828f350c910b88817c6
John Eriksson <johnreriksson@gmail.com>
2013-08-08 13:52:16 +0200

    Fixed scheduleapp

--- functions.js
+++ functions.js
@@ -1,7 +1,11 @@
 var fetchSchedule = function(){
-	var url = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=85420/sv-se&type=3&id=" + $('#input').val() + "&period=VT&week=&mode=0&printer=1&colors=1&head=1&clock=1&foot=1&day=0&width=1240&height=1753&count=1&decrypt=0";
+	var url = "http://www.novasoftware.se/ImgGen/schedulegenerator.aspx?format=png&schoolid=85420/sv-se&type=3&id=" + $('#input').val() + "&period="+ $('#select').val() +"&week=&mode=0&printer=0&colors=1&head=1&clock=1&foot=1&day=0&width="+ $('#container').width() +"&height=800&count=1&decrypt=0";
 	$('#frame').attr('src', url);
 	if ($('#input').val().length == 10) {
 		$('#frame').slideDown();
-	};
+	}else{
+		var url = "error.html";
+		$('#frame').attr('src', url);
+		$('#frame').show();
+	}
 };
--- getschedule.php
+++ getschedule.php
@@ -1,5 +1,19 @@
 <script type="text/javascript" src="[insert_php] echo get_template_directory_uri();[/insert_php]/functions.js"></script>
-<form>
-	Personnummer: <input id="input" type="text" placeholder="XXMMDDXXXX">
-</form>
-<iframe id="frame" src="" style="width:100%; height:1755px; border:0px; display:none"></iframe>
+<div id="container" class="container">
+	<div class="row">
+		<div class="span1">Personnummer:</div>
+		<div class="span2">
+			<input class="span2" id="input" type="text"  placeholder="YYMMDDXXXX">
+		</div>
+		<div class="span1">
+			<select id="select" class="span1">
+				<option value="HT">HT</option>
+				<option value="VT">VT</option>
+			</select>
+		</div>
+		<div class="span2">
+			<button class="btn btn-warning" onclick="fetchSchedule(); return false" style="float:left;">Hämta!</button>
+		</div>
+	</div>
+</div>
+<img id="frame" style="display: none;" src="">
