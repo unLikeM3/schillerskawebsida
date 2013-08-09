63f95e4b873998ac6a81db8a04fe65c3adb2c2f6
John Eriksson <johnreriksson@gmail.com>
2013-08-09 12:51:28 +0200

    I have no idea what I did here Oo

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 			<div class="navbar-inner mh40">
 				<div class="container">
 					<!--<img class="logga brand" src="<?php echo get_template_directory_uri();?>/img/logga2.png">-->
-					<p class="brand"><a href="<?php echo home_url(); ?>">Schillerskas Elevkår</a></p>
+					<p class="brand"><a id="brand"> href="<?php echo home_url(); ?>">Schillerskas Elevkår</a></p>
 					<ul class="nav">
 						<li><a href="<?php echo home_url(); ?>">Hem</a></li>
 						<li><a href="<?php echo post_permalink(2); ?>">Bli Medlem</a></li>
