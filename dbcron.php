a0586f7dfd40e316e2c41ac6ba5a31e0eb99df59
John Eriksson <johnreriksson@gmail.com>
2013-08-20 17:01:48 +0200

    Fixed cron for now, updated member-list, TODO: cron

--- dbcron.php
+++ dbcron.php
@@ -1,19 +1,19 @@
 <?php 
 	require 'connect.php';
 
-	$sql1	= "SELECT * FROM medlemmar";
-	while ($row = mysql_fetch_assoc(mysql_query($sql1))) {
-		$newyear = $row['arskurs'];
+	$sql1	= mysql_query("SELECT * FROM medlemmar");
+	while ($row = mysql_fetch_assoc($sql1)) {
+		$newyear = $row['arskurs'] +1;
 		$persnr = $row['persnr'];
-		//if(!mysql_query("UPDATE medlemmar SET arskurs='$newyear' WHERE persnr='$persnr'")){
-		//	die("Error updating year");
-		//}else{
-			echo "<br>Persnr: " . $persnr . "<br>Arskurs: " . $arskurs;
-		//}
+		
+		if($newyear > 3){
+			$delsql = "DELETE FROM medlemmar WHERE persnr='$persnr'";
+		}else{
+			if(!mysql_query("UPDATE medlemmar SET arskurs='$newyear' WHERE persnr='$persnr'")){
+				die("Error updating year");
+			}else{
+				echo "<br>Persnr: " . $persnr . "<br>Arskurs: " . $newyear;
+			}
+		}
 	}
-
-	echo "<br>" . mysql_num_rows(mysql_query($sql1))
-
-
-
 ?>
