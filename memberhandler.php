96f0e44f1cd047e7596ecd86e80b042b641237af
John Eriksson <johnreriksson@gmail.com>
2013-08-21 19:58:48 +0200

    Fixed telefonnr for 'Bli Medlem'

--- memberhandler.php
+++ memberhandler.php
@@ -7,22 +7,27 @@
 	$pnr = $_POST['persnr'];
 	$email = $_POST['email'];
 	$arskurs = $_POST['arskurs'];
+	$telnr = $_POST['telnr'];
 
-	$sql = "INSERT INTO medlemmar VALUES ('$fname', '$lname', '$pnr', '$email', '$arskurs')";
+	$sql = "INSERT INTO medlemmar VALUES ('$fname', '$lname', '$pnr', '$email', '$arskurs', '$telnr')";
 
 	if($fname != null && $lname != null){
 		if (is_numeric($pnr)) {
-			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				if (mysql_query($sql)) {
-					echo "Du är nu medlem! <br> Klicka <a href='../../../'>här</a> för att gå vidare!";
+			if(is_numeric($telnr)){
+				if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+					if (mysql_query($sql)) {
+						echo "Du är nu medlem! <br> Klicka <a href='../../../'>här</a> för att gå vidare!";
+					}else{
+						die("Final: " . mysql_error());
+					}
 				}else{
-					die("Final: " . mysql_error());
+					die('Du måste fylla i din email!');
 				}
 			}else{
-				die('Du måste fylla i din email!');
+				die("Felaktigt telefonnr");
 			}
-		}else {
-			die('Personnummer måste vara i YYYYMMDDXXXX-format!');
+			}else {
+				die('Personnummer måste vara i YYYYMMDDXXXX-format!');
 		}
 	}else{
 		die('Du måste fylla i förnamn/efternamn!');
